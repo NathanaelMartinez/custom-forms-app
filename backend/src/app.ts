1f3f59e5e1989617f239108a24e203b986c94c6e
NathanaelMartinez <nathanaellmartinez@gmail.com>
2024-10-04 21:22:29 -0600

    config cors for local development

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,6 +1,7 @@
 import "reflect-metadata";
 import express from "express";
 import cors from "cors";
+import dotenv from "dotenv";
 import authRoutes from "./routes/auth-routes";
 import { initializeAuth } from "./services/auth-service";
 import templateRouter from "./routes/template-routes";
@@ -8,14 +9,30 @@ import adminRouter from "./routes/admin-routes";
 
 const app = express();
 
+// get .env variables
+dotenv.config();
+
+// allow local development
+const allowedOrigins = [
+  "http://localhost:5173",
+  process.env.FRONTEND_URL || "http://localhost:5173",
+];
+
 // cors config
-app.use(
-  cors({
-    origin: "https://custom-forms-app-1.onrender.com",
-    methods: ["GET", "POST", "PATCH", "DELETE"],
-    allowedHeaders: ["Authorization", "Content-Type"],
-  })
-);
+const corsOptions: cors.CorsOptions = {
+  origin: (origin, callback) => {
+    // Postman has no origin
+    if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  methods: ["GET", "POST", "DELETE", "PATCH"],
+  credentials: true,
+};
+
+app.use(cors(corsOptions));
 
 // middleware
 app.use(express.json());
