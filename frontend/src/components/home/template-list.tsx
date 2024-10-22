585203927a307cb077bd236c4d72ad0d0d998580
NathanaelMartinez <nathanaellmartinez@gmail.com>
2024-10-22 00:04:37 -0600

    format template cards

--- frontend/src/components/home/template-list.tsx
+++ frontend/src/components/home/template-list.tsx
@@ -1,9 +1,14 @@
-// components/TemplateList.tsx
 import React from "react";
 import { useNavigate } from "react-router-dom";
 import { Card, Row, Col } from "react-bootstrap";
 import { format } from "date-fns";
 import { Template } from "../../types";
+import {
+  ChatDots,
+  ChatLeft,
+  Clipboard2Check,
+  Heart,
+} from "react-bootstrap-icons";
 
 interface TemplateListProps {
   title: string;
@@ -32,14 +37,34 @@ const TemplateList: React.FC<TemplateListProps> = ({
             <Col key={template.id} className="mb-4">
               <Card
                 className="template-card"
-                onClick={() => handleCardClick(template.id)} 
+                onClick={() => handleCardClick(template.id)}
                 style={{ cursor: "pointer" }} // make pointer indicate clickable
               >
+                <Card.Img
+                  variant="top"
+                  src="https://placehold.co/600x820/f4f4f4/FFF?text=Template"
+                  alt="Template placeholder"
+                />
                 <Card.ImgOverlay className="d-flex flex-column justify-content-end">
                   <Card.Title className="fw-bold">{template.title}</Card.Title>
                   <Card.Text>
-                    Filled Forms: {template.responses?.length}
+                    {/* display first few words of description */}
+                    {template.description?.split(" ").slice(0, 5).join(" ")}...
                   </Card.Text>
+                  <div className="d-flex justify-content-between align-items-center mt-2">
+                    <div className="d-flex align-items-center">
+                      <Clipboard2Check className="me-1" />
+                      <span>{template.responses?.length}</span>
+                    </div>
+                    <div className="d-flex align-items-center">
+                      <ChatLeft className="me-1" />
+                      <span>{template.comments?.length}</span>
+                    </div>
+                    <div className="d-flex align-items-center">
+                      <Heart className="me-1" color="red" />
+                      <span>{template.likes}</span>
+                    </div>
+                  </div>
                 </Card.ImgOverlay>
               </Card>
             </Col>
@@ -55,12 +80,31 @@ const TemplateList: React.FC<TemplateListProps> = ({
                 onClick={() => handleCardClick(template.id)}
                 style={{ cursor: "pointer" }}
               >
+                <Card.Img
+                  variant="top"
+                  src="https://placehold.co/600x820/f4f4f4/FFF?text=Template"
+                  alt="Template placeholder"
+                />
                 <Card.ImgOverlay className="d-flex flex-column justify-content-end">
                   <Card.Title className="fw-bold">{template.title}</Card.Title>
                   <Card.Text>
                     Uploaded:{" "}
                     {format(new Date(template.createdAt), "MM-dd-yyyy")}
                   </Card.Text>
+                  <div className="d-flex justify-content-between align-items-center mt-2">
+                    <div className="d-flex align-items-center">
+                      <Clipboard2Check className="me-1" />
+                      <span>{template.responses?.length}</span>
+                    </div>
+                    <div className="d-flex align-items-center">
+                      <ChatDots className="me-1" />
+                      <span>{template.comments?.length}</span>
+                    </div>
+                    <div className="d-flex align-items-center">
+                      <Heart className="me-1" color="red" />
+                      <span>{template.likes}</span>
+                    </div>
+                  </div>
                 </Card.ImgOverlay>
               </Card>
             </Col>
