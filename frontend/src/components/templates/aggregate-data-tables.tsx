9f6d5db49f5e885e15951bc08d740a551a9dbf2f
NathanaelMartinez <nathanaellmartinez@gmail.com>
2024-10-23 14:15:34 -0600

    improve looks of data tables

--- frontend/src/components/templates/aggregate-data-tables.tsx
+++ frontend/src/components/templates/aggregate-data-tables.tsx
@@ -1,5 +1,6 @@
 import React from "react";
-import { AggregatedData } from "../../types"; // Assuming this is where the types are defined
+import { AggregatedData } from "../../types";
+import { Card } from "react-bootstrap";
 
 interface AggregatedDataTablesProps {
   aggregatedData: AggregatedData;
@@ -13,102 +14,108 @@ const AggregatedDataTables: React.FC<AggregatedDataTablesProps> = ({
     return num !== undefined ? num.toFixed(decimals) : "N/A";
   };
 
-  // Safely handle if data is not in expected format
+  // handle if data is not in expected object format
   const hasData = (data?: object) => {
     return data && Object.keys(data).length > 0;
   };
 
   return (
     <div className="text-start">
-      <h2 className="text-dark mt-5">{aggregatedData.responseCount} Responses</h2>
+          <h2 className="text-dark">{aggregatedData.responseCount} Responses</h2>
 
-      {/* Check if numeric data exists */}
+      {/* check if numeric data exists */}
       {hasData(aggregatedData.numericData) && (
-        <div className="table-responsive">
-          <h3 className="text-dark mt-5">Numeric Data</h3>
-          <table className="table table-bordered mt-4">
-            <thead>
-              <tr>
-                <th>Question</th>
-                <th>Average</th>
-                <th>Min</th>
-                <th>Max</th>
-              </tr>
-            </thead>
-            <tbody>
-              {Object.keys(aggregatedData.numericData).map((questionId) => (
-                <tr key={questionId}>
-                  <td>{aggregatedData.numericData[questionId].questionText}</td>
-                  <td>{formatNumber(aggregatedData.numericData[questionId].average)}</td>
-                  <td>{formatNumber(aggregatedData.numericData[questionId].min)}</td>
-                  <td>{formatNumber(aggregatedData.numericData[questionId].max)}</td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
-        </div>
+        <Card className="p-4 custom-card shadow-sm mt-3">
+            <div className="table-responsive">
+              <h3 className="text-dark">Numeric Data</h3>
+              <table className="table table-bordered mt-4">
+                <thead>
+                  <tr>
+                    <th>Question</th>
+                    <th>Average</th>
+                    <th>Min</th>
+                    <th>Max</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {Object.keys(aggregatedData.numericData).map((questionId) => (
+                    <tr key={questionId}>
+                      <td>{aggregatedData.numericData[questionId].questionText}</td>
+                      <td>{formatNumber(aggregatedData.numericData[questionId].average)}</td>
+                      <td>{formatNumber(aggregatedData.numericData[questionId].min)}</td>
+                      <td>{formatNumber(aggregatedData.numericData[questionId].max)}</td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+            </div>
+        </Card>
       )}
 
-      {/* Check if text data exists */}
+      {/* check if text data exists */}
       {hasData(aggregatedData.textData) && (
-        <div className="table-responsive">
-          <h3 className="text-dark mt-5">Text Responses</h3>
-          <table className="table table-bordered mt-4">
-            <thead>
-              <tr>
-                <th>Question</th>
-                <th>Text Counts</th>
-              </tr>
-            </thead>
-            <tbody>
-              {Object.keys(aggregatedData.textData).map((questionId) => (
-                <tr key={questionId}>
-                  <td>{aggregatedData.textData[questionId].questionText}</td>
-                  <td>
-                    {Object.entries(aggregatedData.textData[questionId].counts || {}).map(
-                      ([answer, count]) => (
-                        <p key={answer}>
-                          <strong>{answer}:</strong> {count}
-                        </p>
-                      )
-                    )}
-                  </td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
-        </div>
+        <Card className="p-4 custom-card shadow-sm mt-3">
+            <div className="table-responsive">
+              <h3 className="text-dark">Text Responses</h3>
+              <table className="table table-bordered mt-4">
+                <thead>
+                  <tr>
+                    <th>Question</th>
+                    <th>Text Counts</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {Object.keys(aggregatedData.textData).map((questionId) => (
+                    <tr key={questionId}>
+                      <td>{aggregatedData.textData[questionId].questionText}</td>
+                      <td>
+                        {Object.entries(aggregatedData.textData[questionId].counts || {}).map(
+                          ([answer, count]) => (
+                            <p key={answer}>
+                              <strong>{answer}:</strong> {count}
+                            </p>
+                          )
+                        )}
+                      </td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+            </div>
+        </Card>
       )}
 
-      {/* Check if checkbox data exists */}
+      {/* check if checkbox data exists */}
       {hasData(aggregatedData.checkboxData) && (
-        <div className="table-responsive">
-          <h3 className="text-dark mt-5">Checkbox Option Counts</h3>
-          <table className="table table-bordered mt-4">
-            <thead>
-              <tr>
-                <th>Question</th>
-                <th>Option Counts</th>
-              </tr>
-            </thead>
-            <tbody>
-              {Object.keys(aggregatedData.checkboxData).map((questionId) => (
-                <tr key={questionId}>
-                  <td>{aggregatedData.checkboxData[questionId].questionText}</td>
-                  <td>
-                    {Object.entries(
-                      aggregatedData.checkboxData[questionId].optionCounts || {}
-                    ).map(([option, count]) => (
-                      <p key={option}>
-                        <strong>{option}:</strong> {count}
-                      </p>
-                    ))}
-                  </td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
-        </div>
+        <Card className="p-4 custom-card shadow-sm mt-3">
+            <div className="table-responsive">
+              <h3 className="text-dark">Checkbox Option Counts</h3>
+              <table className="table table-bordered mt-4">
+                <thead>
+                  <tr>
+                    <th>Question</th>
+                    <th>Option Counts</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {Object.keys(aggregatedData.checkboxData).map((questionId) => (
+                    <tr key={questionId}>
+                      <td>{aggregatedData.checkboxData[questionId].questionText}</td>
+                      <td>
+                        {Object.entries(
+                          aggregatedData.checkboxData[questionId].optionCounts || {}
+                        ).map(([option, count]) => (
+                          <p key={option}>
+                            <strong>{option}:</strong> {count}
+                          </p>
+                        ))}
+                      </td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+            </div>
+        </Card>
       )}
     </div>
   );
